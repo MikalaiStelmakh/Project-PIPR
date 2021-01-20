a49079e94d99b102280e76f455961c9ec17370fa
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2021-01-20 17:05:53 +0100

    Merged gui and menu files, changed ReadMe

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
+from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk, ImageDraw
 from ui import UI
-from menu import Menu
 from inputProcessing import InputProcessing
 
 
@@ -71,6 +71,68 @@ class MainWindow(tk.Frame):
         self.text = self.getInput()
         self.commands_data.append(self.text)
         InputProcessing(self.ui, self, self.text)
+
+
+class Menu:
+    def __init__(self, ui, gui):
+        self.ui = ui
+        self.gui = gui
+
+    def cleanUp(self):
+        self.ui.canvas_for_image.delete('all')
+        self.ui.canvas_for_image.delete(self.ui.imagesprite)
+        self.gui.initialValues()
+        self.gui.createImage()
+        self.ui.label.config(text='Hello')
+
+    def openFile(self):
+        file = filedialog.askopenfilename(filetypes=[('txt files', '*.txt')])
+        if file:
+            self.cleanUp()
+            with open(file, 'r') as file_handle:
+                for line in file_handle:
+                    line = line.rstrip()
+                    self.gui.text = line
+                    self.gui.commands_data.append(self.gui.text)
+                    InputProcessing(self.ui, self.gui, self.gui.text)
+            errors = self.gui.errors
+            if errors > 0:
+                message = (
+                    f'{errors} {"errors" if errors > 1 else "error"} '
+                    'occured while opening the file'
+                    )
+                messagebox.showwarning('Warning', message)
+            self.ui.label.config(text=file)
+
+    def saveImage(self):
+        filename = filedialog.asksaveasfilename(
+            filetypes=(
+                ('jpeg files', '*.jpg'),
+                ('png files', '*.png'),
+                ('gif files', '*.gif'),
+                ('all types', '*.*')
+            )
+        )
+        if filename:
+            self.ui.pil_image.save(filename)
+
+    def saveTxt(self):
+        filename = filedialog.asksaveasfilename(
+            filetypes=[('txt files', '*.txt')])
+        if filename:
+            with open(filename, 'w') as file_text:
+                for command in self.gui.commands_data:
+                    file_text.write(command + '\n')
+
+    def userGuide(self):
+        pass
+
+    def forDevelopers(self):
+        pass
+
+    def exit(self):
+        self.ui.master.destroy()
+        self.ui.pil_image.close()
 
 
 def guiMain():
